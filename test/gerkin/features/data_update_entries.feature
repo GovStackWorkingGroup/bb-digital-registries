70cb0c7cd466520c44ef3c09fe0012a72a679f95
Karolina Kopacz <karolina.kopacz94@gmail.com>
2022-12-29 17:28:42 +0100

    TECH-110: Added cucumber tests for data update-entries

--- test/gerkin/features/data_update_entries.feature
+++ test/gerkin/features/data_update_entries.feature
@@ -1,22 +1,19 @@
 Feature: API endpoint allowing users to update multiple records in the Digital Registries database.
   Request endpoint: PUT /data/{registryname}/{versionnumber}/update-entries
 
-  Background:
-    Given The user wants to update multiple records in the Digital Registries database
-
   Scenario: The user successfully updates two existing records in the Digital Registries database
-    Given The records exist in the database
-    When The user triggers an action to update two records in the database
-    And The request with a valid payload is sent
-    Then Operation finishes successfully
+    Given The user wants to update multiple records with first name "Alfie" in the Digital Registries database
+    And The records with first name "Alfie" exist in the database
+    When The user triggers an action to update records with first name "Alfie" in the database
+    Then Operation to update records with first name "Alfie" finishes successfully
 
   Scenario: The user is not able to update two records which do not exist in the Digital Registries database
-    Given The records do not exist in the database
-    When The user triggers an action to update two records in the database
-    And The request with a valid payload is sent
-    Then Operation results in an error
+    Given The user wants to update multiple records with first name "Jerry" in the Digital Registries database
+    And The records with first name "Jerry" do not exist in the database
+    When The user triggers an action to update records with first name "Jerry" in the database
+    Then Operation results to update records  with first name "Jerry" is an error
 
-  Scenario: The user is not able to update two records in the Digital Registries database
-    When The user triggers an action to update two records in the database
-    And The request with an invalid payload is sent
-    Then Operation results in an error
+  Scenario: The user is not able to update two records in the Digital Registries database because of an invalid request
+    Given The user wants to update multiple records with name "Jasmine" in the Digital Registries database
+    When The user triggers an action to update records with name "Jasmine" in the database
+    Then Operation results to update record with name "Jasmine" is an error
---
+++ test/gerkin/features/steps/data_update_entries.js
@@ -0,0 +1,150 @@
+const pactum = require('pactum');
+const { Given, When, Then, Before, After } = require('@cucumber/cucumber');
+const { header, localhost } = require('./helpers/helpers');
+
+let userVariables = {};
+let specDataUpdateEntries;
+
+const baseUrl = `${localhost}data/registry1/version1/update-entries`;
+
+Before(() => {
+  specDataUpdateEntries = pactum.spec();
+});
+
+// Scenario: The user successfully updates two existing records in the Digital Registries database
+Given(
+  'The user wants to update multiple records with first name "Alfie" in the Digital Registries database',
+  () => {
+    userVariables = {
+      ID: 'EE378627348834',
+      FirstName: 'Alfie',
+      LastName: 'Grande',
+      BirthCertificateID: 'RR-1234567889',
+    };
+
+    return userVariables;
+  }
+);
+
+Given('The records with first name "Alfie" exist in the database', () => {
+  return 'The records with first name "Alfie" exist in the database';
+});
+
+When(
+  'The user triggers an action to update records with first name "Alfie" in the database',
+  () => {
+    specDataUpdateEntries
+      .put(`${baseUrl}`)
+      .withHeaders(`${header.key}`, `${header.value}`)
+      .withBody({
+        query: {
+          content: userVariables,
+        },
+        write: {
+          content: userVariables,
+        },
+      });
+  }
+);
+
+Then(
+  'Operation to update records with first name "Alfie" finishes successfully',
+  async () => {
+    await specDataUpdateEntries.toss();
+    specDataUpdateEntries.response().should.have.status(200);
+  }
+);
+
+// Scenario: The user is not able to update two records which do not exist in the Digital Registries database
+Given(
+  'The user wants to update multiple records with first name "Jerry" in the Digital Registries database',
+  () => {
+    userVariables = {
+      ID: 'EE378627348834',
+      FirstName: 'Jerry',
+      LastName: 'Blake',
+      BirthCertificateID: 'RR-1234567889',
+    };
+
+    return userVariables;
+  }
+);
+
+Given(
+  'The records with first name "Jerry" do not exist in the database',
+  () => {
+    return 'The records with first name "Jerry" do not exist in the database';
+  }
+);
+
+When(
+  'The user triggers an action to update records with first name "Jerry" in the database',
+  () => {
+    specDataUpdateEntries
+      .put(`${baseUrl}`)
+      .withHeaders(`${header.key}`, `${header.value}`)
+      .withBody({
+        query: {
+          content: userVariables,
+        },
+        write: {
+          content: userVariables,
+        },
+      });
+  }
+);
+
+Then(
+  'Operation results to update records  with first name "Jerry" is an error',
+  async () => {
+    await specDataUpdateEntries.toss();
+    specDataUpdateEntries.response().should.have.status(404);
+    specDataUpdateEntries
+      .response()
+      .should.have.body('{\n  "Record matching query not found."\n}');
+  }
+);
+
+// Scenario: The user is not able to update two records in the Digital Registries database because of an invalid request
+Given(
+  'The user wants to update multiple records with name "Jasmine" in the Digital Registries database',
+  () => {
+    userVariables = {
+      ID: 'EE378627348834',
+      FirstName: 'Jasmine',
+      LastName: 'Sun',
+      BirthCertificateID: 'RR-1234567889',
+    };
+
+    return userVariables;
+  }
+);
+
+When(
+  'The user triggers an action to update records with name "Jasmine" in the database',
+  () => {
+    specDataUpdateEntries
+      .put(`${baseUrl}`)
+      .withHeaders(`${header.key}`, `${header.value}`)
+      .withBody({
+        query: {
+          content: userVariables,
+        },
+      });
+  }
+);
+
+Then(
+  'Operation results to update record with name "Jasmine" is an error',
+  async () => {
+    await specDataUpdateEntries.toss();
+    specDataUpdateEntries.response().should.have.status(400);
+    specDataUpdateEntries
+      .response()
+      .should.have.body('{\n  "Query not provided."\n}');
+  }
+);
+
+After(() => {
+  specDataUpdateEntries.end();
+});
