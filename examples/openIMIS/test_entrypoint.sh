205734c359fcdad265cedae8c4c27484b65570d9
lruzicki <lruzicki@gmail.com>
2023-06-14 16:39:36 +0200

    TECH-714: added check for service avability

--- examples/openIMIS/test_entrypoint.sh
+++ examples/openIMIS/test_entrypoint.sh
@@ -2,6 +2,7 @@
 docker-compose build db backend
 docker-compose up -d db backend
 
-# exec inserted script in the container
+docker-compose exec backend chmod +x /openimis-be/openIMIS/custom_scripts/check_service_availability.sh
+docker-compose exec backend bash /openimis-be/openIMIS/custom_scripts/check_service_availability.sh
 docker-compose exec backend python /openimis-be/openIMIS/custom_scripts/import_data.py
 
