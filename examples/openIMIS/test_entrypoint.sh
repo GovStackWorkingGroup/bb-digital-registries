ae62f8a70710224d685dd968d0ea0525f8613ae6
lruzicki <lruzicki@gmail.com>
2023-06-14 14:56:41 +0200

    TECH-714: openIMIS backend runs with -d flag

--- examples/openIMIS/test_entrypoint.sh
+++ examples/openIMIS/test_entrypoint.sh
@@ -2,6 +2,9 @@
 docker-compose build db backend
 docker-compose up -d db backend
 
+# exec inserted script in the container
+#docker-compose exec backend python /openimis-be/openIMIS/custom_scripts/import_data.py
+
 docker-compose exec backend chmod +x /check_service_availability.sh
 docker-compose exec backend bash /check_service_availability.sh
 docker-compose exec backend python /import_data.py
