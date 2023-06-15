a3f025187255ee70322b3ab664ab2050f23676e6
lruzicki <lruzicki@gmail.com>
2023-06-15 17:07:59 +0200

    TECH-714: changed db to postgresql

--- examples/openIMIS/test_entrypoint.sh
+++ examples/openIMIS/test_entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 docker-compose build db backend
-docker-compose up -d db backend
-
-docker-compose exec backend chmod +x /openimis-be/openIMIS/custom_scripts/check_service_availability.sh
-docker-compose exec backend bash /openimis-be/openIMIS/custom_scripts/check_service_availability.sh
-docker-compose exec backend python /openimis-be/openIMIS/custom_scripts/import_data.py
+docker-compose up db backend
 
 docker-compose exec backend chmod +x /check_service_availability.sh
 docker-compose exec backend bash /check_service_availability.sh
