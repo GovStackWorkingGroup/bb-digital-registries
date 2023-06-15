f6ce3b974dbb7657f302025b58650e3c585cd00b
lruzicki <lruzicki@gmail.com>
2023-06-15 17:29:01 +0200

    TECH-714: added -d flag

--- examples/openIMIS/test_entrypoint.sh
+++ examples/openIMIS/test_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker-compose build db backend
-docker-compose up db backend
+docker-compose up -d db backend
 
 docker-compose exec backend chmod +x /check_service_availability.sh
 docker-compose exec backend bash /check_service_availability.sh
