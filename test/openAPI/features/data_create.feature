11bc55866c542b016f775982ddfaba138dbe1a26
lruzicki <lruzicki@gmail.com>
2023-07-11 09:30:48 +0200

    TECH-760: added custom ID for single scenario for test suite

--- test/openAPI/features/data_create.feature
+++ test/openAPI/features/data_create.feature
@@ -6,7 +6,7 @@ Feature: API endpoint that allows users to create a new record in the database.
 
     Given The user wants to create a new record in the database
     When User sends POST request with given Information-Mediator-Client header, body, "registryname" as registryname and "111" as versionnumber
-    And User provides body with parameters: ${ID_1} as ID, "Anna" as Firstname, "Stock" as LastName, "RR-1234567999" BirthCertificateID
+    And User provides body with parameters: "${ID_1}" as ID, "Anna" as Firstname, "Stock" as LastName, "RR-1234567993" BirthCertificateID
     Then User receives a response from the POST /data/{registryname}/{versionnumber}/create endpoint
     And The POST /data/{registryname}/{versionnumber}/create endpoint response should be returned in a timely manner 15000ms
     And The POST /data/{registryname}/{versionnumber}/create endpoint response should have status 200
@@ -27,7 +27,7 @@ Feature: API endpoint that allows users to create a new record in the database.
 
     Examples: Valid data
     | ID         |Firstname | LastName | BirthCertificateID
-    | EE34534123 | Zofia    | Don      | RR-5465679229
-    | EE38778473 | Elsa     | Smith    | RR-1234537999
+    | ${ID_2} | Zofia    | Don      | RR-5465679229
+    | ${ID_3} | Elsa     | Smith    | RR-1234537999
     | RR87483474 | Nick     | Evans    | RR-7898797999
     | PP84848484 | Thomas   | Johnson  | RR-8534567781
--- test/openAPI/features/support/data_create.js
+++ test/openAPI/features/support/data_create.js
@@ -10,11 +10,13 @@ const {
   contentTypeHeader,
   dataCreateResponseSchema,
 } = require('./helpers/helpers');
+const data = require('../../testDefaultParameters.json');
 
 chai.use(require('chai-json-schema'));
 
 let specDataCreate;
-
+let dataStore = {};
+let variable3;
 const baseUrl = localhost + dataCreateEndpoint;
 const endpointTag = { tags: `@endpoint=/${dataCreateEndpoint}` };
 
@@ -25,8 +27,17 @@ Before(endpointTag, () => {
 // Scenario: The user successfully creates a record in the database smoke test type
 Given(
   'The user wants to create a new record in the database',
-  () => 'The user wants to create a new record in the database'
+  () => {dataStore['ID_1'] = "This works"}
 );
+
+Given('the user has ID_1 equal to {string}}', function (string) {
+  let actualID_1 = string === "${ID_1}" ? "tak" : "nie";
+  dataStore['ID_1'] = "This doesnt work";
+});
+
+Given('the user sets abc value to 123', function () {
+  variable3 = "XDXDXD";
+});
 
 When(
   'User sends POST request with given Information-Mediator-Client header, body, {string} as registryname and {string} as versionnumber',
@@ -42,13 +53,15 @@ When(
 
 When(
   'User provides body with parameters: {string} as ID, {string} as Firstname, {string} as LastName, {string} BirthCertificateID',
-  (ID, Firstname, LastName, BirthCertificateID) =>
-    specDataCreate.withBody({
-      ID: ID,
+  function (ID, Firstname, LastName, BirthCertificateID) {
+    let actualID = (ID === '${ID_1}') ? dataStore['ID_1'] : dataStore['ID_1'];
+    return specDataCreate.withBody({
+      ID: actualID,
       Firstname: Firstname,
       LastName: LastName,
       BirthCertificateID: BirthCertificateID,
-    })
+    });
+  }
 );
 
 Then(
