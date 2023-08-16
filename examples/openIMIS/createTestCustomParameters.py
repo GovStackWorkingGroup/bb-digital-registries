0022a4252bddb852a512015ed3e086003f46e2a1
Damian Borowiecki <dborowiecki@soldevelo.com>
2023-08-16 21:36:00 +0200

    Added mock csrf and jwt tokens

--- examples/openIMIS/createTestCustomParameters.py
+++ examples/openIMIS/createTestCustomParameters.py
@@ -1,12 +1,13 @@
 import requests
 import json
-
+import os 
 class CustomParamManager:
     def __init__(self):
         self.params = {}
 
     def fetch_tokens(self):
-        response = requests.post("http://localhost:3333/login/")
+        response = requests.post("http://localhost:3333/api/login/")
+        print(response)
         response_data = response.json()
         self.params["csrf_token"] = response_data["csrf_token"]
         self.params["jwt_token"] = response_data["jwt_token"]
@@ -24,38 +25,50 @@ class CustomParamManager:
 
 
 if __name__ == "__main__":
-    print("Creating custom parameters for test suite")
-    param_manager = CustomParamManager()
-    # param_manager.fetch_tokens()
+    try:
+        print("Creating custom parameters for test suite")
+        param_manager = CustomParamManager()
+        # param_manager.fetch_tokens()
 
-    id_parameters = [{"ID_" + str(i): str(2000 + i - 1)} for i in range(1, 35)]
+        id_parameters = [{"ID_" + str(i): str(2000 + i - 1)} for i in range(1, 35)]
 
-    first_name_parameters = [
-        {"FirstName_0": "Anna"},
-        {"FirstName_1": "Zofia"},
-        {"FirstName_2": "Elsa"},
-        {"FirstName_3": "Nick"},
-        {"FirstName_4": "Thomas"},
-    ]
+        first_name_parameters = [
+            {"FirstName_0": "Anna"},
+            {"FirstName_1": "Zofia"},
+            {"FirstName_2": "Elsa"},
+            {"FirstName_3": "Nick"},
+            {"FirstName_4": "Thomas"},
+        ]
 
-    last_name_parameters = [
-        {"LastName_0": "Stock"},
-        {"LastName_1": "Don"},
-        {"LastName_2": "Smith"},
-        {"LastName_3": "Evans"},
-        {"LastName_4": "Johnson"},
-    ]
+        last_name_parameters = [
+            {"LastName_0": "Stock"},
+            {"LastName_1": "Don"},
+            {"LastName_2": "Smith"},
+            {"LastName_3": "Evans"},
+            {"LastName_4": "Johnson"},
+        ]
 
-    birth_certificate_id = [
-        {"BirthCertificateID_0": "RR-1234567999"},
-        {"BirthCertificateID_1": "RR-5465679229"},
-        {"BirthCertificateID_2": "RR-1234537999"},
-        {"BirthCertificateID_3": "RR-7898797999"},
-        {"BirthCertificateID_4": "RR-8534567781"},
-    ]
+        birth_certificate_id = [
+            {"BirthCertificateID_0": "RR-1234567999"},
+            {"BirthCertificateID_1": "RR-5465679229"},
+            {"BirthCertificateID_2": "RR-1234537999"},
+            {"BirthCertificateID_3": "RR-7898797999"},
+            {"BirthCertificateID_4": "RR-8534567781"},
+        ]
 
-    param_manager.add_params(id_parameters)
-    param_manager.add_params(first_name_parameters)
-    param_manager.add_params(last_name_parameters)
-    param_manager.add_params(birth_certificate_id)
-    param_manager.save_to_file()
+        request = {
+            "csrf_token": "D2222",
+            "jwt_token": "D2223",
+        }
+        param_manager.add_params(id_parameters)
+        param_manager.add_params(first_name_parameters)
+        param_manager.add_params(last_name_parameters)
+        param_manager.add_params(birth_certificate_id)
+                self.params["csrf_token"] = response_data["csrf_token"]
+        self.params["jwt_token"] = response_data["jwt_token"]
+        param_manager.save_to_file()
+        print("Custom parameters created.")
+    except Exception as e: 
+        import traceback
+        print("Failed to create custom parameters. Error:")
+        traceback.print_exc()
