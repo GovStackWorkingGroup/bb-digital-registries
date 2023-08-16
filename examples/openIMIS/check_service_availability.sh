7172c2d0f080499e5e0f7ed499e4d4e0d463d698
Damian Borowiecki <dborowiecki@soldevelo.com>
2023-08-16 19:51:48 +0200

    Await API avilable

---
+++ examples/openIMIS/check_service_availability.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+apt-get install -y netcat
+
+check_service() {
+  local service=$1
+  local port=$2
+  local max_retries=10
+  local try=0
+
+  while [ $try -lt $max_retries ]; do
+    nc -z $service $port
+    result=$?
+
+    if [ $result -eq 0 ]; then
+      echo "$service is up!"
+      return 0
+    else
+      echo "Waiting for $service..."
+      sleep 20
+    fi
+
+    try=$((try + 1))
+  done
+
+  echo "$service failed to start in time."
+  return 1
+}
+
+check_service backend 8000
