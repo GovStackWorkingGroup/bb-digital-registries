b26a852bc397b744e9f021696c9de9b41e83876a
Damian Borowiecki <dborowiecki@soldevelo.com>
2023-08-16 21:15:56 +0200

    Replace sleep with API availabilty check

--- examples/openIMIS/test_entrypoint.sh
+++ examples/openIMIS/test_entrypoint.sh
@@ -19,10 +19,7 @@ if [ $SECONDS -gt $timeout ]; then
     exit 1
 fi
 
-docker-compose exec backend chmod +x /check_service_availability.sh
-sleep 60
-#  docker-compose exec backend bash /check_service_availability.sh
-
+chmod u+x ./check_service_availability.sh && ./check_service_availability.sh
 docker-compose logs backend
 docker-compose exec backend python /createTestCustomParameters.py
 container_id=$(docker-compose ps -q backend)
